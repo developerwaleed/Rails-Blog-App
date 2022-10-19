333ce14e343789cfaa0e7cbe35a57c5ee78bd790
Waleed Amjad <waleedamjad99@hotmail.com>
2022-10-19 21:30:39 +0500

    Add method for Comment model

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,7 @@ class Post < ApplicationRecord
     has_many :comments, foreign_key: 'post_id'
     has_many :likes, foreign_key: 'post_id'
     belongs_to :author, class_name: 'User'
+    after_save :update_posts_counter
 
     def update_posts_counter
         author.increment!(:posts_counter)
