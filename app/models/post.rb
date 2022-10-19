788328de29866942d65b442e71646a05c1875dde
Waleed Amjad <waleedamjad99@hotmail.com>
2022-10-19 21:27:55 +0500

    Add method for Post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,13 @@ class Post < ApplicationRecord
     has_many :comments, foreign_key: 'post_id'
     has_many :likes, foreign_key: 'post_id'
     belongs_to :author, class_name: 'User'
+
+    def update_posts_counter
+        author.increment!(:posts_counter)
+    end
+
+    def fetch_recent_comments
+       comments.order('created_at DESC').last(5)
+    end
+
 end
