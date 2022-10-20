42f10436deda105b387bed0b43f9fddd7693650a
Waleed Amjad <waleedamjad99@hotmail.com>
2022-10-20 19:04:00 +0500

    Add validations for post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,10 @@ class Post < ApplicationRecord
   has_many :likes, foreign_key: 'post_id'
   belongs_to :author, class_name: 'User'
   after_save :update_posts_counter
+  validates :title, presence: true
+  validates :title, length: { maximum: 250 }
+  validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :likess_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   def update_posts_counter
     author.increment!(:posts_counter)
