e96ccf7e5e5cc99d88ac4872f260775b584276cf
Waleed Amjad <waleedamjad99@hotmail.com>
2022-10-27 02:30:21 +0500

    Fix linter errors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,10 @@
 class UsersController < ApplicationController
   def index
     @users = User.all
-   end
+  end
 
   def show
     @user_id = params[:id]
     @user = User.find(params[:id].to_i)
-
-end
+  end
 end
