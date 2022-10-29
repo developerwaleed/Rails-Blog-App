6638cf36d8723f242642065975d7f67e909e8275
Waleed Amjad <waleedamjad99@hotmail.com>
2022-10-29 13:54:27 +0500

    Add controllers for post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,4 +6,35 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
+
+  def new
+    newpost = Post.new
+    respond_to do |format|
+      format.html { render :new, locals: { newpost: } }
+    end
+  end
+
+  def create
+    @post = current_user.posts.new(post_params)
+    @post.likes_counter = 0
+    @post.comments_counter = 0
+
+    respond_to do |format|
+      format.html do
+        if @post.save
+          @post.update_posts_counter
+          flash[:success] = 'Post saved successfully'
+          redirect_to user_post_path(current_user, @post.id)
+        else
+          newpost = Post.new
+          flash.now[:error] = 'Error: post could not be saved'
+          render :new, locals: { newpost: }
+        end
+      end
+    end
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :text)
+  end
 end
