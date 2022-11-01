0a6a50ca53b658c789211ee69e441f093b3f8c08
Waleed Amjad <waleedamjad99@hotmail.com>
2022-11-01 18:08:59 +0500

    Fix views specs

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   def index
-    @user = User.includes(posts: [:comments]).find(params[:user_id].to_i)
+    @user = User.includes(posts: [:comments, :author]).find(params[:user_id].to_i)
   end
 
   def show
