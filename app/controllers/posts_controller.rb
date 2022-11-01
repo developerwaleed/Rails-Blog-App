84f778b067160599c027e164ff1b7d1b85da33a7
Waleed Amjad <waleedamjad99@hotmail.com>
2022-11-02 00:36:54 +0500

    Fix eager loading error

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,12 @@
 class PostsController < ApplicationController
   def index
-    @user = User.includes(posts: %i[comments author]).find(params[:user_id].to_i)
+    @user = User.find(params[:user_id])
     @posts = @user.posts.includes(:comments)
   end
 
   def show
-    @post = Post.find(params[:id])
+    @user = User.find(params[:user_id])
+    @post = @user.posts.find(params[:id])
   end
 
   def new
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -7,6 +7,12 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   config.after_initialize do
+    Bullet.enable        = true
+    Bullet.bullet_logger = true
+    Bullet.raise         = true # raise an error if n+1 query occurs
+  end
+
+  config.after_initialize do
     # Bullet.enable        = true
     # Bullet.bullet_logger = true
     # Bullet.raise         = true # raise an error if n+1 query occurs
